df35729601bda48d1da470f4f5dcb074c0973411
faith nenkai <nenkaifaith1@gmail.com>
2019-11-29 10:55:19 +0300

    Set conditions for male akan names

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ function checkName(){
   var cc = parseInt(yyyy.slice(0,2));
   var yy = parseInt(yyyy.slice(2,4));
 
+  var akan;
+
   if(document.getElementsById("inlineRadioMale").checked){
     var gender = "Male";
   }else if(document.getElementsById("inlineRadioFemale").checked){
@@ -27,10 +29,24 @@ function checkName(){
     alert("Please enter a valid month");
     break;
   }
-  
+
 
   var f = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
 
-
+  if(f<1.5 && gender=="Male"){
+    akan="Kwasi"
+  }else if(f>=1.5 && f<2.5 && gender=="Male"){
+    akan="Kwadwo"
+  }else if(f>=2.5 && f<3.5 && gender=="Male"){
+    akan="Kwabena"
+  }else if(f>=3.5 && f<4.5 && gender=="Male"){
+    akan="Kwaku"
+  }else if(f>=4.5 && f<5.5 && gender=="Male"){
+    akan="Yaw"
+  }else if(f>=5.5 && f<6.5 && gender=="Male"){
+    akan="Kofi"
+  }else if(f>=6.5 && f<7.5 && gender=="Male"){
+    akan="Kwame"
+  }
 
   }
