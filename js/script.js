6d3ec520d0de9257953bced7eaf5eb9934c7b60d
faith nenkai <nenkaifaith1@gmail.com>
2019-11-29 14:50:40 +0300

    Move the submit button down

--- js/script.js
+++ js/script.js
@@ -1,66 +1,58 @@
 function checkName(){
-  var dd = document.getElementsByName("dd");
-  var mm = document.getElementsByName("mm");
-  var yyyy = document.getElementsByName("yyyy").toString();
+   var dd, mm , yyyy;
+    dd = document.getElementById("dd").value;
+    mm = document.getElementById("mm").value;
+    yyyy = document.getElementById("yyyy").value;
 
-  var cc = parseInt(yyyy.slice(0,2));
-  var yy = parseInt(yyyy.slice(2,4));
+    var cc = parseInt(yyyy.slice(0,2));
+    var yy = parseInt(yyyy.slice(2,4));
+   
 
-  var akan;
+    if(document.getElementById("inlineRadioMale").checked){
+        gender = "Male";
+    }else if(document.getElementById("inlineRadioFemale").checked){
+        gender = "Female";
+    }else(alert("Please choose gender"));
+    
+    if(dd<1 && dd>31){
+       alert("Please enter a valid date");
+    }
 
-  if(document.getElementsById("inlineRadioMale").checked){
-    var gender = "Male";
-  }else if(document.getElementsById("inlineRadioFemale").checked){
-    var gender = "Female";
-  }else{
-    alert("Please choose gender")
-  }
+    if(mm<1 && mm>12){
+      alert("Please enter a valid month");
+    }
 
-  if(dd>=1 && dd<=31){
-    continue;
-  }else{
-    alert("Please enter a valid date");
-    break;
-  }
+    var f =  ( (cc/4) -((2*cc)-1) + ((5*yy)/4) + (26*(mm+1)/10) + dd ) % 7;
+    alert(f)
+  
+if(f<6.1120 && gender === "Female"){
+  alert("Akosua")
+ }else if(f<6.1130 && gender === "Female"){
+  alert("Adwao")
+ }else if(f<6.1140 && gender === "Female"){
+   alert("Abenna")
+ }else if(f<6.1150 && gender === "Female"){
+   alert("Akua")
+ }else if(f<6.1160 && gender === "Female"){
+  alert("Yaa")
+ }else if(f<6.1170 && gender === "Female"){
+  alert("Afua")
+ }else if(f<6.1180 && gender === "Female"){
+  alert("Ama")
+ }else if(f<6.1120 && gender === "Male"){
+  alert("Kwasi")
+ }else if(f<6.1130 && gender === "Male"){
+  alert("Kwadwo")
+ }else if(f<6.1140 && gender === "Male"){
+   alert("Kwabena")
+ }else if(f<6.1150 && gender === "Male"){
+   alert("Kwaku")
+ }else if(f<6.1160 && gender === "Male"){
+  alert("Yaw")
+ }else if(f<6.1170 && gender === "Male"){
+  alert("kofi")
+ }else if(f<6.1180 && gender === "Male"){
+  alert("Kwame")
+ } 
+};
 
-  if(mm>=1 && mm<=12){
-    continue;
-  }else{
-    alert("Please enter a valid month");
-    break;
-  }
-
-
-  var f = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7;
-
-  if(f<1.5 && gender=="Male"){
-    akan="Kwasi"
-  }else if(f>=1.5 && f<2.5 && gender=="Male"){
-    akan="Kwadwo"
-  }else if(f>=2.5 && f<3.5 && gender=="Male"){
-    akan="Kwabena"
-  }else if(f>=3.5 && f<4.5 && gender=="Male"){
-    akan="Kwaku"
-  }else if(f>=4.5 && f<5.5 && gender=="Male"){
-    akan="Yaw"
-  }else if(f>=5.5 && f<6.5 && gender=="Male"){
-    akan="Kofi"
-  }else if(f>=6.5 && f<7.5 && gender=="Male"){
-    akan="Kwame"
-  }else if(f,1.5 && gender=="Female"){
-    akan="Akosua"
-  }else if(f>=1.5 && f<2.5 && gender=="Female"){
-    akan="Adwoa"
-  }else if(f>=2.5 && f<3.5 && gender=="Female"){
-    akan="Abenna"
-  }else if(f>=3.5 && f<4.5 && gender=="Female"){
-    akan="Akua"
-  }else if(f>=4.5 && f<5.5 && gender=="Female"){
-    akan="Yaa"
-  }else if(f>=5.5 && f<6.5 && gender=="Female"){
-    akan="Afua"
-  }else if(f>=6.5 && f<7.5 && gender=="Female"){
-    akan="Ama"
-  }
-  return(alert(akan));
-}
